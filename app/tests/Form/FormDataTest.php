c9caea99e5163f61901c177a2cb229e722d846df
mwojcikbs <mwojcikbs@gmail.com>
2025-01-13 20:03:34 +0100

    refactor test

--- app/tests/Form/FormDataTest.php
+++ app/tests/Form/FormDataTest.php
@@ -2,40 +2,21 @@
 
 namespace App\Tests\Form;
 
+use App\Form\UserForm;
 use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\Forms;
 use Symfony\Component\Form\Test\TypeTestCase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use App\Form\UserForm;
 use Symfony\Component\Validator\Validation;
 
 class FormDataTest extends TypeTestCase
 {
 	protected FormFactoryInterface $formFactory;
 
-	/**
-	 * @return array[]
-	 */
-	public static function formDataProvider(): array
+	public static function nameLastnameProvider(): array
 	{
-		$validImagePath   = sys_get_temp_dir() . '/validImage.png';
-		$invalidMimePath  = sys_get_temp_dir() . '/invalidMime.txt';
-		$tooLargeImagePath= sys_get_temp_dir() . '/tooLargeImage.png';
-
-		if (!file_exists($validImagePath)) {
-			file_put_contents($validImagePath, str_repeat('A', 1024 * 1024));
-		}
-
-		if (!file_exists($invalidMimePath)) {
-			file_put_contents($invalidMimePath, 'Some text content');
-		}
-
-		if (!file_exists($tooLargeImagePath)) {
-			file_put_contents($tooLargeImagePath, str_repeat('A', 3 * 1024 * 1024));
-		}
-
 		return [
 			'valid no file' => [
 				'formData' => [
@@ -45,7 +26,6 @@ class FormDataTest extends TypeTestCase
 				],
 				'isValid' => true,
 			],
-
 			'empty lastname' => [
 				'formData' => [
 					'name'      => 'Mateusz',
@@ -54,7 +34,6 @@ class FormDataTest extends TypeTestCase
 				],
 				'isValid' => false,
 			],
-
 			'empty name' => [
 				'formData' => [
 					'name'      => '',
@@ -63,41 +42,30 @@ class FormDataTest extends TypeTestCase
 				],
 				'isValid' => false,
 			],
+		];
+	}
 
-			'valid data with file' => [
-				'formData' => [
-					'name'      => 'Mateusz',
-					'lastname'  => 'Wojcik',
-					'attachment' => new UploadedFile(
-						$validImagePath,
-						'validImage.png',
-						'image/png',
-						null,
-						true
-					),
-				],
-				'isValid' => true,
-			],
+	public static function fileConstraintsProvider(): array
+	{
+		$validImagePath   = sys_get_temp_dir() . '/validImage.png';
+		$invalidMimePath  = sys_get_temp_dir() . '/invalidMime.txt';
+		$tooLargeImagePath= sys_get_temp_dir() . '/tooLargeImage.png';
 
-			'too large attachment' => [
-				'formData' => [
-					'name'      => 'Mateusz',
-					'lastname'  => 'Wojcik',
-					'attachment' => new UploadedFile(
-						$tooLargeImagePath,
-						'tooLargeImage.png',
-						'image/png',
-						null,
-						true
-					),
-				],
-				'isValid' => false,
-			],
+		if (!file_exists($validImagePath)) {
+			file_put_contents($validImagePath, str_repeat('A', 1024 * 1024));
+		}
+		if (!file_exists($invalidMimePath)) {
+			file_put_contents($invalidMimePath, 'Some text content');
+		}
+		if (!file_exists($tooLargeImagePath)) {
+			file_put_contents($tooLargeImagePath, str_repeat('A', 3 * 1024 * 1024));
+		}
 
+		return [
 			'invalid mime type' => [
 				'formData' => [
-					'name'      => 'Mateusz',
-					'lastname'  => 'Wojcik',
+					'name'     => 'Mateusz',
+					'lastname' => 'Wojcik',
 					'attachment' => new UploadedFile(
 						$invalidMimePath,
 						'invalidMime.txt',
@@ -106,7 +74,21 @@ class FormDataTest extends TypeTestCase
 						true
 					),
 				],
-				'isValid' => false,
+				'expectedError' => "attachment:\n    ERROR: Please upload a valid image file (JPEG, PNG, GIF).\n",
+			],
+			'too large attachment' => [
+				'formData' => [
+					'name'     => 'Mateusz',
+					'lastname' => 'Wojcik',
+					'attachment' => new UploadedFile(
+						$tooLargeImagePath,
+						'tooLargeImage.png',
+						'image/png',
+						null,
+						true
+					),
+				],
+				'expectedError' => "attachment:\n    ERROR: The file is too large (3.15 MB). Allowed maximum size is 2 MB.\n",
 			],
 		];
 	}
@@ -125,33 +107,39 @@ class FormDataTest extends TypeTestCase
 
 	public static function tearDownAfterClass(): void
 	{
-		$validImagePath = sys_get_temp_dir() . '/validImage.png';
-		$invalidMimePath = sys_get_temp_dir() . '/invalidMime.txt';
-		$tooLargeImagePath = sys_get_temp_dir() . '/tooLargeImage.png';
+		$validImagePath   = sys_get_temp_dir() . '/validImage.png';
+		$invalidMimePath  = sys_get_temp_dir() . '/invalidMime.txt';
+		$tooLargeImagePath= sys_get_temp_dir() . '/tooLargeImage.png';
 
-		if (file_exists($validImagePath)) {
-			unlink($validImagePath);
-		}
-
-		if (file_exists($invalidMimePath)) {
-			unlink($invalidMimePath);
-		}
-
-		if (file_exists($tooLargeImagePath)) {
-			unlink($tooLargeImagePath);
+		foreach ([$validImagePath, $invalidMimePath, $tooLargeImagePath] as $file) {
+			if (file_exists($file)) {
+				unlink($file);
+			}
 		}
 
 		parent::tearDownAfterClass();
 	}
 
 	/**
-	 * @dataProvider formDataProvider
+	 * @dataProvider nameLastnameProvider
 	 */
-	public function testSubmitValidData(array $formData, bool $isValid): void
+	public function testNameLastname(array $formData, bool $isValid): void
 	{
 		$form = $this->formFactory->create(UserForm::class);
 		$form->submit($formData);
 
 		$this->assertEquals($isValid, $form->isValid());
+	}
+
+	/**
+	 * @dataProvider fileConstraintsProvider
+	 */
+	public function testFileConstraints(array $formData, ?string $expectedError): void
+	{
+		$form = $this->formFactory->create(UserForm::class);
+		$form->submit($formData);
+
+		$errorsString = (string) $form->getErrors(true, false);
+		$this->assertStringContainsString($expectedError, $errorsString);
 	}
 }
